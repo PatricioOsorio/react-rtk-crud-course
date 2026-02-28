6cd8e8d1992499d5f6fe61c85aa1a9b719dbfada
Patricio Miguel Osorio Osorio <patricioosorio@Patricios-MacBook-Pro.local>
2026-02-28 17:28:45 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <button className="btn btn-primary">Button here</button>
+      <button className="btn btn-secondary">Button here</button>
+      <button className="btn btn-warning">Button here</button>
+    </>
+  );
+}
+
+export default App;
